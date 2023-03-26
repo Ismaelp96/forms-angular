75c47940f1569c062e45061964b5296641a2bf23
Ismael Oliveira <ismael6991@live.com>
2023-03-26 10:07:28 -0300

    Componentizando as mensagens

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,7 +7,8 @@ import { HeaderComponent } from './components/header/header.component'
 import { FooterComponent } from './components/footer/footer.component'
 import { CadastroComponent } from './cadastro/cadastro.component'
 import { SucessoCadastroComponent } from './sucesso-cadastro/sucesso-cadastro.component'
-import { FormsModule } from '@angular/forms'
+import { FormsModule } from '@angular/forms';
+import { MensagemComponent } from './components/mensagem/mensagem.component'
 
 @NgModule({
   declarations: [
@@ -16,6 +17,7 @@ import { FormsModule } from '@angular/forms'
     FooterComponent,
     CadastroComponent,
     SucessoCadastroComponent,
+    MensagemComponent,
   ],
   imports: [BrowserModule, AppRoutingModule, FormsModule],
   providers: [],
